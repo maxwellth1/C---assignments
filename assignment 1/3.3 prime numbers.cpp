d7f774e0125bdbd10bd7f98a9f986be60c04eb50
maxwell <max35243524@gmail.com>
2025-10-24 08:48:55 -0400

    3.3 prime number, leet code test file

--- assignment 1/3.3 prime numbers.cpp
+++ assignment 1/3.3 prime numbers.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main(){
-    
+    int N; int count = 0; int beg = 2;
+    cout << "Enter a number: "; cin >> N;
+
+    while(count < N){
+        for(int y = 2; y < N; y++){
+            bool prime = false;
+            if(beg % y == 0){
+                
+            }
+        }
+
+        count++;
+    }
 }
