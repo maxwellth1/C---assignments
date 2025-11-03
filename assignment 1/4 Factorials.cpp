5fdbabf6223063bd5da30d4e587dd26f8a431d2b
maxwell <max35243524@gmail.com>
2025-11-03 16:55:45 -0500

    4 factorials

---
+++ assignment 1/4 Factorials.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int number; cout << "Enter a number: "; cin >> number;
+    switch(number){
+        case 0:
+        case 1:
+            cout << "1\n";
+            break;
+        case 2:
+            cout << "1\n";
+            break;
+        case 3:
+            cout << "1\n";
+            break;
+        case 4:
+            cout << "1\n";
+            break;
+        case 5:
+            cout << "1\n";
+            break;
+        case 6:
+            cout << "1\n";
+            break;  
+        case 7:
+            cout << "1\n";
+            break;
+        case 8:
+            cout << "1\n";
+            break;
+        case 9:
+            cout << "1\n";
+            break;
+        case 10:
+            cout << "1\n";
+            break;
+        default:
+            cout << "Input not supported\n";
+            break;
+    }
+    return 0;
+}
