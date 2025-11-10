ca0484bb5d9b4f2b9107b5a29851de22981031ec
maxwell <max35243524@gmail.com>
2025-11-10 08:51:08 -0500

    7.5

---
+++ assignment 2/7.5 pointers and strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char *oddOrEven = "Never odd or even";
+    char *nthCharPtr = &oddOrEven[5];
+    nthCharPtr -= 2;
+    cout << *nthCharPtr << "\n";
+    char **pointerPtr =  &nthCharPtr;
+    cout << pointerPtr << "\n";
+    cout << **pointerPtr << "\n";
+    nthCharPtr++;
+    cout << oddOrEven << "\n";
+    cout << nthCharPtr << "\n";
+    cout << "nthCharPtr is: " << nthCharPtr - oddOrEven;
+}
