9edc3edfb5910d056f503001fd8b60ddeb6ed181
maxwell <max35243524@gmail.com>
2025-11-12 16:19:24 -0500

    finished 4, PointArray

--- assignment 3/geometry.cpp
+++ assignment 3/geometry.cpp
@@ -58,7 +58,56 @@ void PointArray :: resize(int n){
     }
 }
 
+void PointArray :: push_back(const Point &p){
+    if(size == capacity){
+        int newCap = (capacity == 0) ? 1 : capacity * 2;
+        Point *newPts = new Point[newCap];
+        for(int x = 0; x < size; x++){
+            newPts[x] = points[x];
+        }
+        delete[] points;
+        points = newPts;
+        capacity = newCap;
+    }
+    points[size + 1] = p;
+}
 
+void PointArray :: clear(){
+    resize(0);
+}
+
+void PointArray :: insert(const int pos, const Point &p){
+    resize(size + 1);
+    for(int x = size - 1; x > pos; x--){
+        points[x] = points[x - 1];
+    }
+    points[pos] = p;
+}
+
+void PointArray :: remove(const int pos){
+    if(pos > 0 && pos < size){
+        for(int x = 0; x < size - 2; x++){
+            points[x] = points[x + 1];
+        }
+        resize(size - 1);
+    }
+}
+
+const int PointArray :: getSize() const{
+    return size;
+}
+
+Point* PointArray :: get(const int pos){
+    if(pos < 0 && pos >= size)
+        return nullptr;
+    return &points[pos];
+}
+
+const Point* PointArray :: get(const int pos) const{
+    if(pos < 0 && pos >= size)
+        return nullptr;
+    return &points[pos];
+}
 
 int main(){
 
--- assignment 3/geometry.h
+++ assignment 3/geometry.h
@@ -6,7 +6,7 @@ class Point{
     private:
         int x, y;
     public:
-        Point(int x = 0, int y = 0){this->x = x; this->y = y;}
+        Point(int x = 0, int y = 0) : x(x), y(y) {}
 
         int getX() const {return x;}
         int getY() const {return y;}
@@ -21,8 +21,18 @@ class PointArray{
         int size; int capacity;
         void resize(int n);
     public:
+    //Constructors
         PointArray();
         PointArray(const Point pts[], const int size);
         PointArray(const PointArray &ca);
         ~PointArray();
+
+    //Functions
+        void push_back(const Point &p);
+        void insert(const int pos, const Point &p);
+        void remove(const int pos);
+        const int getSize() const;
+        void clear();
+        Point* get(const int pos);
+        const Point* get(const int pos) const;
 };
