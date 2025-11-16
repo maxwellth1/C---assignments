0e86be1b157eff1fab03adf87d13639ea8277f06
Max <max35243524@gmail.com>
2025-11-15 19:37:42 -0500

    finished problem set 3

---
+++ assignment3/section 6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+const string vowels = "aeiou";
+
+string pigLatinify(const string s){
+    if(s.size() == 0){return s;}
+    if(s.find("qu") == 0){return s.substr(2, s.size() - 2) + "-" + s.substr(0, 2) + "ay";}
+    else if(vowels.find(s[0]) != string::npos){return s + "way";}
+    else {return s.substr(2, s.size() - 1) + "-" + s[0] + "ay";}
+}
